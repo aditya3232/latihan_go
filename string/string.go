04ca2346db86178311b80ce0f2246a689473ae68
aditya3232 <m.aditya3232@gmail.com>
2023-11-20 12:33:44 +0700

    first commit

---
+++ string/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("halo guys")
+	fmt.Println(len("halo guys"))
+	fmt.Println("halo guys"[1]) // untuk ambil string diposisi, isinya byte dari string
+
+}
---
+++ variable/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var (
+	nama string = "Budi"
+	umur        = 30
+)
+
+func main() {
+
+	fmt.Println(nama, umur)
+
+	nama = "ichsan"
+	umur = 24
+	fmt.Println(nama, umur)
+
+	hobi := "makan"
+	fmt.Println(hobi)
+}
